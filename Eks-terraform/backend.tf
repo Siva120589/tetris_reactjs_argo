52da6c1e2543a52be61c917904b34e5bfa5a1dc2
Siva Kumar <sivakumarjeevavantham@gmail.com>
2025-08-01 18:33:06 +0530

    terraform eks deleted

--- Eks-terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "react-js-tetris-eks-terraform" # Replace with your actual S3 bucket name
-    key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
